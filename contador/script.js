f47000ffc7a7bcee196ac7c40636d43e056131b4
pedrinho abacateiro <119536475+pedr1nhowww@users.noreply.github.com>
2024-03-22 08:12:17 -0300

    Add files via upload

---
+++ contador/script.js
@@ -0,0 +1,23 @@
+const  daysE1 = document.querySelector("#days");
+const  hoursE1 = document.querySelector("#hours");
+const  minutesE1 = document.querySelector("#minutes");
+const  secondsE1 = document.querySelector("#seconds");
+const startBtn  = document.querySelector("#startBtn");
+const pauseBtn  = document.querySelector("#pauseBtn");
+const resumeBtn = document.querySelector("#resumeBtn");
+
+
+let Interval;
+let secondsE1 = 0;
+let minutesE1 = 0;
+let hoursE1 = 0;
+let days = 0;
+let ispaused = false;
+
+.startBtn addEventListener ("click", startTimer)
+
+function startTimer(){
+     Interval setInterval (( ){
+        
+     })
+}
